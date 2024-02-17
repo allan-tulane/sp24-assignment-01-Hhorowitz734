98e1923fab8940a1dec7c54696368ed5a0cf29dd
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-02-17 13:44:58 -0600

    solved longest_run_recursive and longest_run

--- main.py
+++ main.py
@@ -17,8 +17,22 @@ def foo(x):
         return ra + rb
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
+    
+    count = 0
+    m_count = 0
+
+    for num in mylist:
+
+        if (num == key):
+            count += 1
+        
+        else:
+            count = 0
+        
+        m_count = max(m_count, count)
+    
+    return m_count
+
 
 
 class Result:
@@ -45,8 +59,37 @@ def to_value(v):
         return int(v)
         
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
 
-
+    def recurse(start, end): #Recursive helper function
+        if start > end:
+            return Result(0, 0, 0, False)
+        if start == end: #Base case -> We have reached individual elements
+            if mylist[start] == key: #It's the key -> Return 1
+                return Result(1, 1, 1, True)
+            else: #It's not the key -> return 0
+                return Result(0, 0, 0, False)
+        
+        #We send both halves of the list to our recursive function
+        mid = (start + end) // 2
+        left_result = recurse(start, mid)
+        right_result = recurse(mid + 1, end)
+        
+        #Left and right sizes to combine longest_run from both sides
+        left_size = left_result.left_size if mylist[start] == key else 0
+        right_size = right_result.right_size if mylist[end] == key else 0
+        
+        #Combining our halves
+        if left_result.is_entire_range and mylist[mid] == key:
+            left_size = left_result.left_size + right_result.left_size
+        
+        if right_result.is_entire_range and mylist[mid + 1] == key:
+            right_size = right_result.right_size + left_result.right_size
+        
+        longest_size = max(left_result.longest_size, right_result.longest_size, left_result.right_size + right_result.left_size)
+        
+        is_entire_range = left_result.is_entire_range and right_result.is_entire_range and (mylist[mid] == key or mylist[mid + 1] == key)
+        
+        return Result(left_size, right_size, longest_size, is_entire_range)
+    
+    return recurse(0, len(mylist) - 1)
 
